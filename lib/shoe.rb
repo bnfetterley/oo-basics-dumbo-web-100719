498d0a7e4a45ad0f5437be101dfe80c7d9cb0a1d
Brianna Fetterley <bnfetterley@usfca.edu>
2019-10-08 19:45:40 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,14 @@
 class Shoe
   
-  # attr_accessor :author, :page_count, :genre
+  attr_accessor :brand, :color, :size, :material, :condition
   # attr_reader :title
  
   # def initialize(title)
   #   @title = title
   # end
  
-  # def turn_page
-  #   puts "Flipping the page...wow, you read fast!"
-  # end
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
  
 end# Make your shoe class here!
