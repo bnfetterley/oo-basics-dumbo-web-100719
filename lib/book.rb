1f50fb64ead02534ef4c585fa24e1a0ed53c3c39
Brianna Fetterley <bnfetterley@usfca.edu>
2019-10-08 19:40:19 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -15,6 +15,15 @@ class Book
     @author 
   end 
   
+   def page_count=(page_count)
+    @page_count = page_count
+  end 
+  
+  def page_count 
+    @page_count
+  end 
+  
+  
   
   # def property=(something)
   #   @property = something
